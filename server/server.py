a868f1cdfdab7c456e604973d333cb3f62ae1d47
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-09-14 02:45:13 +0800

    A2SG-20 chore: clear linting issues in server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
-import os
+"""
+Module Docstring
+"""
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,8 +10,11 @@ CORS(app)
 
 @app.route("/")
 def hello():
+    """
+    Route Docstring
+    """
     return "Hello World!"
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5101, debug=True)
