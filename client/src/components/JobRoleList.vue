438865e227a16a91c422ebafbaf9223c540c7fde
HZKMaurice <zikaiho@gmail.com>
2023-09-15 21:32:42 +0800

    A2SG-2 feat: added logic and styling to display role listing for HR

--- client/src/components/JobRoleList.vue
+++ client/src/components/JobRoleList.vue
@@ -1,34 +1,63 @@
 <template>
-  <div>
+  <div class="container-fluid mt-5">
     <!-- Conditional rendering for when no job roles are available -->
     <div v-if="jobRoles.length === 0">
-      <p>No job roles available.</p>
+      <p class="text-primary">No job roles available.</p>
     </div>
 
-    <!-- Job role list -->
-    <ul v-else>
-      <li v-for="(jobRole, index) in jobRoles" :key="index" class="job-role-item">
-        {{ jobRole.role_listing_id }}
-        {{ jobRole.role_id }}
-        {{ jobRole.role_listing_desc }}
-        {{ jobRole.role_listing_source }}
-        {{ jobRole.role_listing_open }}
-        {{ jobRole.role_listing_close }}
-        {{ jobRole.role_description }}
-        {{ jobRole.role_name }}
-        {{ jobRole.role_status }}
-        {{ jobRole.role_skills }}
-        <!-- You can display additional job role information here -->
-      </li>
-    </ul>
+    <div v-else class="container card">
+      <!-- Job role list -->
+      <div v-if="userType == 'HR_admin'">
+        <div
+          v-for="(jobRole, index) in jobRoles"
+          :key="index"
+          class="job-role-item mb-4 border-bottom"
+          @click="goToRolePage(index)"
+        >
+          <div class="card-body">
+            <h5 class="card-title no-underline">
+              <a href="#" class="card-link text-normal">{{ jobRole.role_description }}</a>
+              <small
+                v-if="jobRole.role_status == 'active'"
+                class="badge rounded-pill bg-success text-white p-2 ms-2"
+                >{{ jobRole.role_status }}</small
+              >
+              <small v-else class="badge rounded-pill bg-danger text-white p-2 ms-2">{{
+                jobRole.role_status
+              }}</small>
+            </h5>
+
+            <h6 class="card-subtitle mb-2 text-muted">{{ jobRole.role_name }}</h6>
+            <div class="mb-2">
+              <div
+                v-for="(roleSkill, index2) in jobRole.role_skills"
+                :key="index2"
+                class="badge rounded-pill bg-light text-dark p-2 me-2"
+              >
+                {{ roleSkill }}
+              </div>
+            </div>
+            <p class="card-text">
+              {{ jobRole.role_listing_desc }}
+            </p>
+            <div class="row">
+              <small class="text-muted"> Published Date {{ jobRole.role_listing_open }}</small>
+              <small class="text-muted"> Closing Date {{ jobRole.role_listing_close }}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 const jobRoles = ref([])
+const userType = ref('')
 
 onMounted(() => {
+  userType.value = 'HR_admin'
   jobRoles.value = [
     {
       role_listing_id: 1,
@@ -36,7 +65,7 @@ onMounted(() => {
       role_listing_desc: 'This is a sample job role description.',
       role_listing_source: 201,
       role_listing_open: '2023-08-01',
-      role_listing_close: '2023-08-15',
+      role_listing_close: '2023-9-14',
       role_description: 'Learning Facilitator',
       role_name: 'Talent Attraction',
       role_status: 'active',
@@ -48,7 +77,7 @@ onMounted(() => {
       role_listing_desc: 'Join our dynamic team and contribute to our success.',
       role_listing_source: 202,
       role_listing_open: '2023-08-10',
-      role_listing_close: '2023-08-24',
+      role_listing_close: '2023-09-24',
       role_description: 'Software Engineer',
       role_name: 'Engineering',
       role_status: 'active',
@@ -60,7 +89,7 @@ onMounted(() => {
       role_listing_desc: 'Exciting opportunity for a creative mind.',
       role_listing_source: 203,
       role_listing_open: '2023-07-25',
-      role_listing_close: '2023-08-08',
+      role_listing_close: '2023-12-08',
       role_description: 'Graphic Designer',
       role_name: 'Creative Team',
       role_status: 'inactive',
@@ -72,7 +101,7 @@ onMounted(() => {
       role_listing_desc: 'Lead our marketing efforts and make an impact.',
       role_listing_source: 204,
       role_listing_open: '2023-08-05',
-      role_listing_close: '2023-08-19',
+      role_listing_close: '2023-10-19',
       role_description: 'Marketing Manager',
       role_name: 'Marketing',
       role_status: 'active',
@@ -84,7 +113,7 @@ onMounted(() => {
       role_listing_desc: 'Join our finance team and drive financial excellence.',
       role_listing_source: 205,
       role_listing_open: '2023-08-15',
-      role_listing_close: '2023-08-29',
+      role_listing_close: '2023-11-29',
       role_description: 'Financial Analyst',
       role_name: 'Finance',
       role_status: 'active',
@@ -97,7 +126,18 @@ onMounted(() => {
 <style scoped>
 /* Add your component-specific styles here */
 .job-role-item {
-  cursor: pointer;
+  /* cursor: pointer; */
   /* Add any other styling you need */
+}
+:hover .job-role-item {
+  cursor: pointer;
+} /* Remove underline by default */
+.job-role-item .no-underline a {
+  text-decoration: none !important;
+}
+
+/* Apply underline only when hovering over the specific <a> tag */
+.job-role-item:hover .no-underline a {
+  text-decoration: underline !important;
 }
 </style>
