455d0951f35a246dfab1ae4158168ad20c0d8ade
rayquekCW <ray.quek@gmail.com>
2023-09-22 00:27:23 +0800

    A2SG-24 feat: add logic to UpdateRoleListing

--- client/src/views/UpdateRoleListingView.vue
+++ client/src/views/UpdateRoleListingView.vue
@@ -1,39 +1,3 @@
-<script setup>
-import { ref } from 'vue'
-
-const rolenames = [
-  'Scrum Master1',
-  'Product Owner2',
-  'Scrum Master3',
-  'Product Owner4',
-  'Scrum Master5',
-  'Product Owner6',
-  'Scrum Master7',
-  'Product Owner8',
-  'Scrum Master9',
-  'Product Owner10',
-  'Scrum Master11',
-  'Product Owner12'
-]
-
-const searchResults = ref([])
-
-function searchRoleNames(event) {
-  const searchTerm = event.target.value.toLowerCase()
-  if (searchTerm.trim() === '') {
-    searchResults.value = []
-    return
-  }
-  searchResults.value = rolenames.filter((name) => name.toLowerCase().includes(searchTerm))
-}
-
-function fillInputBox(value) {
-  const roleNameInput = document.getElementById('roleName')
-  roleNameInput.value = value
-  searchResults.value = []
-}
-</script>
-
 <template>
   <div class="updateRoleListing container-fluid w-50 text-center">
     <div class="text-start mt-3">
@@ -50,11 +14,9 @@ function fillInputBox(value) {
         <label for="roleListingID" class="form-label">Role Listing ID</label>
         <input
           id="roleListingID"
+          v-model="roleListingID"
           type="text"
           class="form-control role"
-          placeholder=""
-          value=""
-          required=""
           disabled
         />
       </div>
@@ -63,45 +25,27 @@ function fillInputBox(value) {
         <label for="roleName" class="form-label">Role Name</label>
         <input
           id="roleName"
+          v-model="roleName"
           type="text"
           class="form-control"
           placeholder=""
-          value=""
-          required=""
-          @input="searchRoleNames"
+          disabled
         />
-        <div v-if="searchResults.length > 0" class="searchResults">
-          <div
-            v-for="(result, i) in searchResults"
-            :key="i"
-            class="result"
-            @click="fillInputBox(result)"
-          >
-            {{ result }}
-          </div>
-        </div>
       </div>
 
       <div class="mb-3">
         <label for="startDate" class="form-label">Application Start Date</label>
-        <input
-          id="startDate"
-          type="date"
-          class="form-control"
-          placeholder=""
-          value=""
-          required=""
-        />
+        <input id="startDate" v-model="startDate" type="date" class="form-control" placeholder="" />
       </div>
 
       <div class="mb-3">
         <label for="closeDate" class="form-label">Application Close Date</label>
         <input
           id="closeDate"
+          v-model="closeDate"
           type="date"
           class="form-control"
           placeholder=""
-          value=""
           required=""
         />
       </div>
@@ -109,7 +53,7 @@ function fillInputBox(value) {
       <div id="skills" class="mb-3">
         <label for="skills" class="form-label d-block">Skills</label>
         <div
-          v-for="(roleSkill, i) in skills"
+          v-for="(roleSkill, i) in selectedData.skills"
           :key="i"
           class="badge rounded-pill bg-light text-dark p-2 me-2 mb-2"
         >
@@ -119,13 +63,71 @@ function fillInputBox(value) {
 
       <div class="mb-3">
         <label for="textarea">Role Description</label>
-        <textarea id="textarea" class="form-control" rows="5"></textarea>
+        <textarea id="textarea" v-model="roleDescription" class="form-control" rows="5"></textarea>
         <div class="invalid-feedback">Valid last name is required.</div>
       </div>
 
       <div class="mb-3 text-end">
-        <button id="update" class="defaultBtn">Update</button>
+        <button id="update" class="defaultBtn" @click="update()">Update</button>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { updateRoleListing } from '../service/UpdateRoleListing.service'
+
+//to be replace with data sent by other components/page
+
+const selectedData = {
+  role_listing_id: 2,
+  role_name: 'Head, Talent Acquisition',
+  role_id: 234567892,
+  skills: [
+    'Scrum Master1',
+    'Product Owner2',
+    'Scrum Master3',
+    'Product Owner4',
+    'Scrum Master5',
+    'Product Owner6',
+    'Scrum Master7',
+    'Product Owner8',
+    'Scrum Master9',
+    'Product Owner10',
+    'Scrum Master11',
+    'Product Owner12'
+  ],
+  role_listing_desc: 'Test Description',
+  role_listing_open: '2023-09-17',
+  role_listing_close: '2023-09-20'
+}
+
+const roleListingID = ref(selectedData.role_listing_id)
+const roleName = ref(selectedData.role_name)
+const startDate = ref(selectedData.role_listing_open)
+const closeDate = ref(selectedData.role_listing_close)
+const roleDescription = ref(selectedData.role_listing_desc)
+
+function update() {
+  // const updater = currentUser.id (get updater userid)
+
+  const dataToUpdate = {
+    role_listing_id: roleListingID.value,
+    role_id: selectedData.role_id,
+    // role_listing_updater:updater,
+    role_listing_desc: roleDescription.value,
+    role_listing_open: startDate.value,
+    role_listing_close: closeDate.value
+  }
+  console.log(dataToUpdate)
+
+  updateRoleListing(dataToUpdate)
+    .then((result) => {
+      console.log('success' + result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+</script>
