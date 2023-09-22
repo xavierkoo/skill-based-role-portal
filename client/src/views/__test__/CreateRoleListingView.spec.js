d570df033d8405da62771bd52275b5df355631e1
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-09-22 11:28:40 +0800

    tests: add tests for create role listing

---
+++ client/src/views/__test__/CreateRoleListingView.spec.js
@@ -0,0 +1,55 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import CreateRoleListing from '../CreateRoleListingView.vue'
+
+describe('UpdateRoleListing.vue', () => {
+  // Check all elements are rendered
+  it('renders all necessary form elements', () => {
+    const wrapper = mount(CreateRoleListing)
+
+    expect(wrapper.find('#roleListingID').exists()).toBe(true)
+    expect(wrapper.find('#startDate').exists()).toBe(true)
+    expect(wrapper.find('#closeDate').exists()).toBe(true)
+    expect(wrapper.find('#textarea').exists()).toBe(true)
+    expect(wrapper.find('#create').exists()).toBe(true)
+  })
+
+  //role listing id field is disabled
+  it('disables Role Listing ID input field', () => {
+    const wrapper = mount(CreateRoleListing)
+
+    const roleListingIDInput = wrapper.find('#roleListingID')
+    expect(roleListingIDInput.attributes('disabled')).exist
+  })
+
+  //Check if start date input change accordingly
+  it('updates Application Start Date input value', async () => {
+    const wrapper = mount(CreateRoleListing)
+
+    const startDateInput = wrapper.find('#startDate')
+    await startDateInput.setValue('2023-09-22')
+
+    expect(startDateInput.element.value).toBe('2023-09-22')
+  })
+
+  //Check if close date input change accordingly
+  it('updates Application Close Date input value', async () => {
+    const wrapper = mount(CreateRoleListing)
+
+    const closeDateInput = wrapper.find('#closeDate')
+    await closeDateInput.setValue('2023-09-30')
+
+    expect(closeDateInput.element.value).toBe('2023-09-30')
+  })
+
+  //Check if description input change accordingly
+  it('updates Role Description textarea value', async () => {
+    const wrapper = mount(CreateRoleListing)
+
+    const textarea = wrapper.find('#textarea')
+    await textarea.setValue('New Role Description')
+
+    expect(textarea.element.value).toBe('New Role Description')
+  })
+})
