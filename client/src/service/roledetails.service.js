8c53006dd4f7d17f5611e60e59664dc3cc89eac6
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-09-22 11:28:59 +0800

    feat: add create role listing UI and functionality

---
+++ client/src/service/roledetails.service.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:8080/api/v1/'
+
+export const fetchRoleDetails = async () => {
+  return (await axios.get(`${BASE_URL}roledetails/`)).data
+}
--- client/src/service/rolelisting.service.js
+++ client/src/service/rolelisting.service.js
@@ -6,6 +6,10 @@ export const fetchRoleListings = async () => {
   return (await axios.get(`${BASE_URL}rolelistings/`)).data
 }
 
+export const createRoleListing = async (role) => {
+  return await axios.post(`${BASE_URL}rolelistings/create`, role)
+}
+
 // export const createUser = async (user) => {
 //   return (await axios.post(`${BASE_URL}/users`, user)).data
 // }
