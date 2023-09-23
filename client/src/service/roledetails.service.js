0fe92d63dab47548f2ef9c91dbd4a9a3223288ad
a2ronk <aaronkwah@gmail.com>
2023-09-23 16:43:52 +0800

    A2SG-39 fix: typo on endpoint naming

--- client/src/service/roledetails.service.js
+++ client/src/service/roledetails.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080/api/v1/'
+const BASE_URL = 'http://localhost:8080/api/v1/roledetails/'
 
 export const fetchRoleDetails = async () => {
   return (await axios.get(`${BASE_URL}`)).data
