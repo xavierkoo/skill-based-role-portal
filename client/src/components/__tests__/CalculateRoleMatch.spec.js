e30b280811ce06f7672ff7d79c669396f75e7fa6
HZKMaurice <zikaiho@gmail.com>
2023-09-28 20:17:46 +0800

    A2SG-50 test: add unit test for CalculateRoleMatch

---
+++ client/src/components/__tests__/CalculateRoleMatch.spec.js
@@ -0,0 +1,45 @@
+import { mount } from '@vue/test-utils'
+import CalculateRoleMatch from '../CalculateRoleMatch.vue'
+import { vi, describe, it, expect } from 'vitest'
+import { getStaffSkills } from '../../service/staffskills.service'
+
+vi.mock('../../service/staffskills.service') // Update with the correct path to your service
+
+describe('CalculateRoleMatch', () => {
+  it('displays 0% match when no staff skills are found', async () => {
+    const roleSkills = {
+      role_skills: ['skill1', 'skill2', 'skill3']
+    }
+    const staffSkills = []
+    getStaffSkills.mockResolvedValue({ data: staffSkills })
+
+    const wrapper = mount(CalculateRoleMatch, {
+      props: { roleSkills }
+    })
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.text()).toContain('0 % match')
+    expect(wrapper.find('.text-warning').exists()).toBe(false)
+    expect(wrapper.find('.text-success').exists()).toBe(false)
+    expect(wrapper.find('.text-danger').exists()).toBe(true)
+  })
+
+  it('handles errors when fetching staff skills', async () => {
+    const roleSkills = {
+      role_skills: ['skill1', 'skill2', 'skill3']
+    }
+    getStaffSkills.mockRejectedValue(new Error('Failed to fetch staff skills'))
+
+    const wrapper = mount(CalculateRoleMatch, {
+      props: { roleSkills }
+    })
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.text()).toContain('0 % match')
+    expect(wrapper.find('.text-warning').exists()).toBe(false)
+    expect(wrapper.find('.text-success').exists()).toBe(false)
+    expect(wrapper.find('.text-danger').exists()).toBe(true)
+  })
+})
