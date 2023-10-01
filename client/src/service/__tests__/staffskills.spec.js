287349cfae672be8adec1adfd6a293430fc234f8
HZKMaurice <zikaiho@gmail.com>
2023-10-01 21:45:57 +0800

    A2SG-50 fix: service unit test

--- client/src/service/__tests__/staffskills.spec.js
+++ client/src/service/__tests__/staffskills.spec.js
@@ -10,20 +10,54 @@ describe('getStaffSkills', () => {
   })
 
   it('should return staff skills for a valid staff ID', async () => {
-    const id = 123 // Replace with a valid staff ID
+    const id = 123456789 // Replace with a valid staff ID
     const response = {
       status: 200,
-      data: {
-        /* Replace with sample data */
-      }
+      data: [
+        {
+          staff_id: 123456789,
+          skill_id: 345678790,
+          ss_status: 'active',
+          skill_name: 'Certified Scrum Professional'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678866,
+          ss_status: 'active',
+          skill_name: 'Certified Scrum Developer'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678890,
+          ss_status: 'unverified',
+          skill_name: 'Certified Scrum@Scale Practitioner'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678913,
+          ss_status: 'active',
+          skill_name: 'Python Programming'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678927,
+          ss_status: 'in-progress',
+          skill_name: 'Certified Scrum Coach'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678935,
+          ss_status: 'in-progress',
+          skill_name: 'Certified Scrum Trainer'
+        }
+      ]
     }
     axios.get.mockResolvedValueOnce(response)
-
     const result = await getStaffSkills(id)
 
     expect(axios.get).toHaveBeenCalledTimes(1)
     expect(axios.get).toHaveBeenCalledWith(`${'http://localhost:8080/api/v1/staffskills'}/${id}`)
-    expect(result).toEqual(response)
+    expect(result).toEqual(response.data)
   })
 
   it('should throw an error for an invalid staff ID', async () => {
