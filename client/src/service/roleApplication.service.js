b8a6eecf6985d26958e48e2673bf49d0658a50a2
a2ronk <aaronkwah@gmail.com>
2023-10-07 10:09:57 +0800

    A2SG-54 feat: add GET Roleapplications services

--- client/src/service/roleApplication.service.js
+++ client/src/service/roleApplication.service.js
@@ -5,3 +5,11 @@ const BASE_URL = 'http://localhost:8080/api/v1/applications/'
 export const createRoleApplication = async (applicationData) => {
   return await axios.post(`${BASE_URL}`, applicationData)
 }
+
+export const getRoleApplications = async () => {
+  return await axios.get(`${BASE_URL}`)
+}
+
+export const getRoleApplicationById = async (staffID) => {
+  return await axios.get(`${BASE_URL}/${staffID}`)
+}
