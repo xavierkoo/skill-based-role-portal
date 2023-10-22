75101b472e44437931a3d70264edb822c0086261
rayquekCW <ray.quek@gmail.com>
2023-10-22 17:39:23 +0800

    A2SG-121 test: add visualize workflow test for hr

--- client/src/components/__tests__/RoleDetails.spec.js
+++ client/src/components/__tests__/RoleDetails.spec.js
@@ -104,6 +104,105 @@ describe('RoleDetails', () => {
     mock.restore()
   })
 
+  it('Verify the workflow of viewing detailed information for open roles (hr)', async () => {
+    localStorage.setItem('id', 123456788)
+    const mock = new MockAdapter(axios)
+    mock.onGet('http://localhost:8080/api/v1/staffdetails/123456788').reply(200, {
+      Results: [
+        {
+          f_name: 'AH GAO',
+          l_email: 'TAN',
+          email: 'tan_ah_gao@all-in-one.com.sg',
+          biz_address: '60 Paya Lebar Rd, #06-33 Paya Lebar Square, Singapore 409051',
+          sys_role: 'hr',
+          dept: 'FINANCE',
+          staff_id: 123456788,
+          phone: '65-1234-5678'
+        }
+      ]
+    })
+    mock.onGet('http://localhost:8080/api/v1/staffskills/123456788').reply(200, {
+      Results: [
+        {
+          staff_id: 123456789,
+          skill_id: 345678790,
+          ss_status: 'active',
+          skill_name: 'Certified Scrum Professional'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678866,
+          ss_status: 'active',
+          skill_name: 'Certified Scrum Developer'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678890,
+          ss_status: 'unverified',
+          skill_name: 'Certified Scrum@Scale Practitioner'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678913,
+          ss_status: 'active',
+          skill_name: 'Python Programming'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678927,
+          ss_status: 'in-progress',
+          skill_name: 'Certified Scrum Coach'
+        },
+        {
+          staff_id: 123456789,
+          skill_id: 345678935,
+          ss_status: 'in-progress',
+          skill_name: 'Certified Scrum Trainer'
+        }
+      ]
+    })
+    const roleDetails = {
+      role_name: 'Head, Talent Attraction',
+      role_listing_desc:
+        "The Head, Talent Attraction is responsible for strategic workforce planning to support the organisation's growth strategies through establishing talent sourcing strategies, determining the philosophy for the selection and securing of candidates and overseeing the onboarding and integration of new hires into the organisation. He/She develops various approaches to meet workforce requirements and designs employer branding strategies. He oversees the selection processes and collaborates with business stakeholders for the hiring of key leadership roles. As a department head, he is responsible for setting the direction and articulating goals and objectives for the team, and driving the integration of Skills Frameworks across the organisation's talent attraction plans.The Head, Talent Attraction is an influential and inspiring leader who adopts a broad perspective in the decisions he makes. He is articulate and displays a genuine passion for motivating and developing his team.",
+      role_listing_open: '10/11/2023',
+      role_listing_close: '24/11/2023',
+      role_skills: [
+        'Pascal Programming',
+        'Python Programming',
+        'Certified Scrum Master',
+        'Product Owner'
+      ],
+      role_listing_creator: ['Vincent Rex', 'colins_vincent_rex@all-in-one.com.sg'],
+      role_listing_updater: ['Vincent Rexs', 'colins_vincent_rex@all-in-one.com.sg']
+    }
+    const wrapper = mount(RoleDetails, {
+      props: {
+        roleDetails: roleDetails
+      }
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    expect(wrapper.find('#CalculateRoleMatch').exists()).toBe(true)
+    expect(wrapper.find('#posted_on').exists()).toBe(true)
+    expect(wrapper.find('#closed_on').exists()).toBe(true)
+    expect(wrapper.find('#skills').exists()).toBe(true)
+    expect(wrapper.find('#responsibilities').exists()).toBe(true)
+    expect(wrapper.find('#descLabel').exists()).toBe(true)
+    expect(wrapper.find('#apply_btn').exists()).toBe(true)
+    expect(wrapper.find('#update_btn').exists()).toBe(true)
+    expect(wrapper.find('#role_name').text()).toContain(roleDetails.role_name)
+    expect(wrapper.find('#desc').text()).toContain(roleDetails.role_listing_desc)
+    expect(wrapper.find('#open').text()).toContain(roleDetails.role_listing_open)
+    expect(wrapper.find('#close').text()).toContain(roleDetails.role_listing_close)
+    expect(wrapper.find('.isPosted').text()).toContain('Updated By')
+    expect(wrapper.find('#creatorUpdater').text()).toContain(roleDetails.role_listing_updater[0])
+    expect(wrapper.text()).toContain('Pascal Programming')
+    expect(wrapper.text()).toContain('Python Programming')
+    expect(wrapper.find('.showMore').exists()).toBe(true)
+    mock.restore()
+  })
+
   it("Visualize the workflow for viewing the alignment between open roles and the staff member's skill set", async () => {
     localStorage.setItem('id', 123456789)
     const mock = new MockAdapter(axios)
