c431df91b9050002298bacd93b18f788817352bc
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-10-28 00:35:30 +0800

    feat: update and add test cases

--- client/cypress/e2e/sourceManagerWorkflow.cy.js
+++ client/cypress/e2e/sourceManagerWorkflow.cy.js
@@ -3,20 +3,36 @@ describe('Source Manager Workflow', () => {
     cy.visit('http://localhost:8080')
   })
 
-  it('should display the login page', () => {
+  it('SM-1: should display the login page', () => {
     cy.get('.login').should('exist')
   })
 
-  it('should log in as an HR and redirect to the role listings page', () => {
+  it('SM-2: should log in as an HR and redirect to the role listings page', () => {
     cy.get('#dropdown').select('Source Manager')
     cy.get('.defaultBtn').click()
   })
 
-  it('should only show View Role Applicants and Application Status in the navbar', () => {
+  it('SM-3: should only show View Role Applicants and Application Status, Company Logo, and Logout in the navbar', () => {
     cy.get('#dropdown').select('Source Manager')
     cy.get('.defaultBtn').click()
     cy.get('.status').should('exist')
     cy.get('.applicants').should('exist')
     cy.get('.create').should('not.exist')
+    cy.get('.logout').should('exist')
+    cy.get('.talentNav').should('exist')
+  })
+
+  it('SM-4: should direct to view role applicants page when view role applicants button is clicked', () => {
+    cy.get('#dropdown').select('Source Manager')
+    cy.get('.defaultBtn').click()
+    cy.get('.active.applicants').click()
+    cy.get('h1').should('exist').contains('Role Applicants')
+  })
+
+  it('SM-5: should logout when logout button is clicked', () => {
+    cy.get('#dropdown').select('HR')
+    cy.get('.defaultBtn').click()
+    cy.get('.nav-link.logout').first().click()
+    cy.get('.login').should('exist')
   })
 })
